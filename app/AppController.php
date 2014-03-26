816e93e04091f7f70f62aef17176269f112f668a
gunjansoni <gunjan.soni2002@gmail.com>
2014-03-26 16:46:23 +0530

    Update AppController.php
    removing test data

--- app/AppController.php
+++ app/AppController.php
@@ -3,17 +3,5 @@
 class AppController extends Controller
 {
 
-    public function beforeFilter()
-    {
-        $settings = Cache::get('all_settings_cache');
-        if (!$settings) {
-            $settings = Setting::all();
-            $settings = Model::result_array($settings);
-            Cache::set('all_settings_cache', $settings);
-        }
-        foreach ($settings as $setting) {
-            Configure::set('setting.' . $setting['name'], $setting['value']);
-        }
-    }
-
+    
 }
